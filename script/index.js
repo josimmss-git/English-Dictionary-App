eb2d5d5d0f87f472befe0c1d2115fbe2dba34f88
josimmss-git <josimmss2021@gmail.com>
2026-03-05 12:37:04 +0600

    all card section  is done according to the figma file

--- script/index.js
+++ script/index.js
@@ -5,6 +5,48 @@ const loadLessons = () => {
   .then((json) => displayLesson(json.data));
 }
 
+const loadLevelWord = (id) => {
+ 
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+  
+  
+};
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+//   {
+//     "id": 102,
+//     "level_no": 2,
+//     "lessonName": "Everyday Words"
+// }
+  
+  
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+  <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+
+      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <p class="font-semibold">Meading/Pronouncialtion</p>
+      <div class="bangla-font text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+      </div>
+
+
+    </div>
+  `;
+    wordContainer.append(card);
+  });
+};
+
+
 
 const displayLesson = (lessons) => {
   // 1.get the container & empty
@@ -29,7 +71,7 @@ const displayLesson = (lessons) => {
     // 4.append into container
     levelContainer.append(btnDiv);
 
-  }
+  };
 };
 
 loadLessons();
